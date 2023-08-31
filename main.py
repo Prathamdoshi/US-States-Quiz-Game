c4a68f32040ddcddd9045769c137320ff43d97d3
Pratham Doshi <37569372+Prathamdoshi@users.noreply.github.com>
2023-08-31 13:19:28 -0700

    Set Up | Complete

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+import turtle
 from turtle import Turtle,Screen
 
+t = Turtle()
+screen = Screen()
+
+
+screen.addshape("blank_states_img.gif")
+t.shape("blank_states_img.gif")
+screen.title("U.S States Game")
+turtle.mainloop()
