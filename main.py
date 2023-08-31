eb0961563fad2c8fb95aa13da6efefcce9d78350
Pratham Doshi <37569372+Prathamdoshi@users.noreply.github.com>
2023-08-31 13:55:42 -0700

    Map Screen Setup | State Words Appearing Completed

--- main.py
+++ main.py
@@ -1,11 +1,48 @@
+# import libraries
 import turtle
-from turtle import Turtle,Screen
+import pandas as pd
+from turtle import Turtle, Screen
 
-t = Turtle()
+# initialize variables from Turtle() & Screen() class
+map = Turtle()
 screen = Screen()
 
+# set properties for the canvas
+image = "blank_states_img.gif"
+screen.addshape(image)
+map.shape(image)
+map.penup()
 
-screen.addshape("blank_states_img.gif")
-t.shape("blank_states_img.gif")
-screen.title("U.S States Game")
+
+# import csv
+state_df = pd.read_csv("50_states.csv")
+states = state_df["state"].tolist()
+
+# loop
+
+while True:
+
+    t1 = Turtle()
+    t1.penup()
+
+    user_input = screen.textinput(title="Total 50", prompt="Please guess the state: ")
+
+    if user_input in states:
+
+        result = state_df[state_df["state"] == user_input]
+        x_cord = result['x'].tolist()[0]
+        y_cord = result['y'].tolist()[0]
+
+        t1.setposition(x=x_cord, y=y_cord)
+        t1.write(user_input)
+
+
+    else:
+
+        print("no match")
+
+
+
+
+
 turtle.mainloop()
